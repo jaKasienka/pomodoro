ec1933eaf013165529071c71481654e7bcd65d49
Katharina Cembik <iokasia@proton.me>
2026-08-08 22:30:10 +0200

    add test script and serve static library files

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/lib', express.static(path.join(__dirname, 'lib')));
 
 app.get('/api/tasks', (_req, res) => {
   res.json(db.getAllTasks());
